59a934b2770692ec5c7cb9e8cdcbc59ff105922c
mecene <mecene.co@gmail.com>
2026-05-12 17:41:28 +0200

    cleaned repo

--- backup.ps1
+++
@@ -1,5 +0,0 @@
-$source = "C:\Users\Legion\Documents\IllyrianCycling\index.html"
-$backupDir = "C:\Users\Legion\Documents\IllyrianCycling\backups"
-if (!(Test-Path $backupDir)) { New-Item -ItemType Directory -Path $backupDir | Out-Null }
-$timestamp = Get-Date -Format "yyyy-MM-dd_HH-mm"
-Copy-Item $source "$backupDir\index_$timestamp.html"
